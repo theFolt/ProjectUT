cec9444e86187923e3030cea398808bd9b1660c8
theFolt <raksogrz@gmail.com>
2023-01-19 15:18:00 +0100

    step9: idk, too much to write

--- login-app/src/App.js
+++ login-app/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes, Outlet } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Outlet, Navigate } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import ErrorPage from './components/ErrorPage';
 import LoginPage from './components/LoginPage';
-import Unauthorize from './components/Unauthorize';
+import { getToken } from './utils/session'
 
 function App() {
   return (
@@ -11,8 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Outlet />}>
           <Route path="" exac element={<LoginPage />} />
-          <Route path="HomePage" element={<HomePage />} />
-          <Route path="Unauthorize" element={<Unauthorize />} />
+          <Route exact path="HomePage" element={getToken() ? <HomePage /> : <Navigate to='/' replace />}/>
           <Route path="*" element={<ErrorPage />} />
         </Route>
       </Routes>
