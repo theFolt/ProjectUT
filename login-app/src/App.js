933b6f4572034ad48a19b784ef906272421a89ce
theFolt <raksogrz@gmail.com>
2023-01-19 14:17:10 +0100

    step2: files configured, it is working

--- login-app/src/App.js
+++ login-app/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Route, Routes, Outlet } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import ErrorPage from './components/ErrorPage';
+import LoginPage from './components/LoginPage';
+import Unauthorize from './components/Unauthorize';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Outlet />}>
+          <Route path="/LoginPage" element={<LoginPage />} />
+          <Route path="/HomePage" element={<HomePage />} />
+          <Route path="/ErrorPage" element={<ErrorPage />} />
+          <Route path="/Unauthorize" element={<Unauthorize />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
